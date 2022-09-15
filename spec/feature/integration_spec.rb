56d2b7fd960a43b7e9bfd5391d402f4193f585ff
root <root@localhost>
2022-09-15 21:47:42 +0000

    Added CRUD for other fields

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,26 +5,24 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Price', with: 5.99
+    fill_in 'book[published_date]', with: Date.new(1997, 6, 26)
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('jk rowling')
+    expect(page).to have_content(5.99)
+    expect(page).to have_content(Date.new(1997, 6, 26))
   end
 
   scenario 'can change input field' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
     fill_in 'Title', with: 'lord of the rings'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('lord of the rings')
-  end
-
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Title', with: 'harry potter'
-    fill_in 'Author', with: 'J.K Rowling'
+    fill_in 'Author', with: 'jk rowling'
     fill_in 'Price', with: 5.99
-    fill_in 'Date_Published', with: Date.new(1997, 6, 26)
+    fill_in 'book[published_date]', with: Date.new(1997, 6, 26)
+    fill_in 'Title', with: 'harry potter'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'JK Rowling', price: 5.99, published_date: Date.new(1997,6,26))
   end
 
   it 'is valid with valid attributes' do
